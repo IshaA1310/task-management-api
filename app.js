1d76fd3d2cc1b095d8527bb806e9b81102a94530
IshaA1310 <127334292+IshaA1310@users.noreply.github.com>
2025-12-05 11:19:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.get('/', (req, res) => {
+  res.send('Task management server is live');
+});
 app.use('/api/users', authRoutes);
 app.use('/api/tasks', taskRoutes);
 
